71b29712d3e370dacfa71e833dccc17eb56e6ca0
Discord615 <vanbeau1@gmail.com>
2021-02-15 18:30:22 +0800

    next commit

--- CodingPackage/vanMain.java
+++ CodingPackage/vanMain.java
@@ -5,6 +5,6 @@ public class vanMain {
         System.out.printf("TEst");
         for (int i = 0; i < 5; i++) {
             System.out.printf("Number of iterations : %d", i + 1); // much testing later
-        }
+        } // !This is kewl
     }
 }
