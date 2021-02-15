4d29751f31a2f9e0d27fe79aa0169f34d2cbe3dc
Discord615 <vanbeau1@gmail.com>
2021-02-15 18:19:26 +0800

    test commit

--- CodingPackage/vanMain.java
+++ CodingPackage/vanMain.java
@@ -2,6 +2,6 @@ package CodingPackage;
 
 public class vanMain {
     public static void main(String[] args) {
-        System.out.printf("TEst");
+        System.out.printf("TEst"); // much setting up is needed
     }
 }
