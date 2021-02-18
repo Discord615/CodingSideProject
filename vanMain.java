6d8c425fdd977d6fbb38c086cfabdfaae600c0c4
Discord615 <vanbeau1@gmail.com>
2021-02-18 16:05:26 +0800

    new commit

--- vanMain.java
+++ vanMain.java
@@ -2,6 +2,6 @@ package codePack;
 
 public class vanMain {
     public static void main(String[] args) {
-        System.out.printf("TEst");
+        System.out.printf("TEst"); //kita ba toh?
     }
 }
