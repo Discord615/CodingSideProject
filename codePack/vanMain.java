c8aa1aa82862572595d23f99ebce6dc10d327eda
Discord615 <vanbeau1@gmail.com>
2021-02-18 16:27:18 +0800

    first stages

--- codePack/vanMain.java
+++ codePack/vanMain.java
@@ -1,7 +1,37 @@
 package codePack;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
+
 public class vanMain {
-    public static void main(String[] args) {
-        System.out.printf("TEst"); //kita ba toh?
+
+    public static void main(String[] args) throws Exception {
+        loginReg();
+    }
+
+    private static void loginReg() throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int choice = 0;
+
+        String line = "-------------------------";
+        while (true) {
+            System.out.printf("%sMenu%s\n[1]Login\n[2]Register\nEnter Choice: ", line, line);
+            try {
+                String ph = in.readLine();
+                choice = Integer.parseInt(ph);
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("test");
+                        break;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Not a number");
+                TimeUnit.MILLISECONDS.sleep(1500);
+                continue;
+            }
+        }
     }
 }
